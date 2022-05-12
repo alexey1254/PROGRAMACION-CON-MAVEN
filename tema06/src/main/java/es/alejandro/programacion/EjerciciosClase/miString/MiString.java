9cafa91bcdad6f309fad980be6011a854a6b8d47
alexey1254 <alxkill54@gmail.com>
2022-05-12 17:35:45 +0100

    Se hacen cambios en clase

---
+++ tema06/src/main/java/es/alejandro/programacion/EjerciciosClase/miString/MiString.java
@@ -0,0 +1,9 @@
+package es.alejandro.programacion.EjerciciosClase.miString;
+
+public class MiString {
+    String cadena;
+    public MiString(String cadena) {
+        this.cadena=cadena;
+    }
+    
+}
