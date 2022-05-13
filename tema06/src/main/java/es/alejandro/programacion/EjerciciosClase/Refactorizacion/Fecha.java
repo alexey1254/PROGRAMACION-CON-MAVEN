aace92fe70d371109f7b0d981c63fdbd787ee4e2
alexey1254 <alxkill54@gmail.com>
2022-05-13 11:39:34 +0100

    Se modifica la clase fecha

--- tema06/src/main/java/es/alejandro/programacion/EjerciciosClase/Refactorizacion/Fecha.java
+++ tema06/src/main/java/es/alejandro/programacion/EjerciciosClase/Refactorizacion/Fecha.java
@@ -2,13 +2,14 @@ package es.alejandro.programacion.EjerciciosClase.Refactorizacion;
 
 public class Fecha {
 
-    private static int diasMeses[] ={31,28,31,30,31,30,31,31,30,31,30,31};
+    private static int diasMeses[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     private int dia;
     private int mes;
     private int anio;
 
     /**
      * Constructor
+     * 
      * @param dia
      * @param mes
      * @param anio
@@ -19,42 +20,54 @@ public class Fecha {
         this.anio = anio;
     }
 
-    public int diasMes() {
-
+    @Override
+    public String toString() {
+        return String.format("Dia: %s, Mes: %s, Año: %s", this.dia, this.mes, this.anio);
     }
 
     /**
      * 
-     * @return
+     * @return Los dias que trae el mes
      */
-    public boolean valida () {
-        if (dia < 1 || dia > 31)
-            return false;
-        if (mes < 1 || mes > 12)
-            return false;
-// determinamos la cantidad de días del mes:
-        int diasMes = 0;
-        switch (mes) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12: diasMes = 31; break;
-            case 4:
-            case 6:
-            case 9:
-            case 11 : diasMes = 30; break;
-            case 2 : // verificación de año bisiesto
-                if ( (anio % 400 == 0) ||
-                        ( (anio % 4 == 0) && (anio % 100 != 0) ) )
-                    diasMes = 29;
-                else diasMes = 28;
-                break;
+    private int diasMes() {
+        return Fecha.diasMeses[mes - 1];
+    }
+
+    /**
+     * Comprueba cuantos dias trae el mes de febrero en caso de que sea bisiesto en
+     * el array se pone que trae 29 dias
+     * 
+     */
+    private void diasBisiesto() {
+        if ((this.anio % 400 == 0) || ((this.anio % 4 == 0) && (this.anio % 100 != 0))) {
+            Fecha.diasMeses[1] = 29;
         }
-        if (dia > diasMes)
+    }
+
+    /**
+     * Comprueba que el rango de fecha sea correcto, en caso contrario los
+     * inicializa a 0
+     * 
+     * @return True si son validos
+     */
+    public boolean valida() {
+        if (this.mes < 1 || this.mes > 12) {
+            this.mes = 0;
             return false;
-        else return true;
+        }
+        if (this.mes == 2) {
+            diasBisiesto();
+        }
+        if (this.dia < 1 || this.dia > diasMes()) {
+            this.dia = 0;
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Fecha f = new Fecha(29, 2, 2007);
+        f.valida();
+
     }
 }
