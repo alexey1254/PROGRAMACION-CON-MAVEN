29c140a52ba512a5cbf77309a441a66caa61194b
alexey1254 <alxkill54@gmail.com>
2022-05-21 16:43:05 +0100

    Se añade el paquete modelo junto con sus respectivas clases

---
+++ tema07/src/main/java/es/alejandro/programacion/modelo/Conexion.java
@@ -0,0 +1,46 @@
+package es.alejandro.programacion.modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
+
+/**
+ * @author Alejandro
+ */
+public class Conexion {
+    private static String BASE;
+    private static String USER;
+    private static final String PASSWORD = "123456";
+    private static final String CONEXION = "";
+
+    /**
+     * 
+     * @return
+     */
+    public static Conexion getConexionFerreteria() {
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static PreparedStatement getPreparedStatement() {
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static ResultSet consulta() {
+
+    }
+
+    /**
+     * 
+     */
+    public static void close() {
+
+    }
+}
---
+++ tema07/src/main/java/es/alejandro/programacion/modelo/ModeloDAO.java
@@ -0,0 +1,37 @@
+package es.alejandro.programacion.modelo;
+
+import java.sql.ResultSet;
+
+/**
+ * @author Alejandro
+ */
+public class ModeloDAO {
+
+    private Producto registroProducto(ResultSet rs) {
+
+    }
+
+    public static int numProductos() {
+
+    }
+
+    public static Producto getProductos(int page, int tamanioPage) {
+
+    }
+
+    public static Producto getProducto(int codigo) {
+
+    }
+
+    public static int actualizarProducto(Producto producto) {
+
+    }
+
+    public static int insertarProducto(Producto producto) {
+
+    }
+
+    public static int borrarProducto(int codigo) {
+
+    }
+}
