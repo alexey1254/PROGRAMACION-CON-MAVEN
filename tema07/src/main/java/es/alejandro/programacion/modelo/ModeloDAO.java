8253ad776a629041ac62bf5d4d3bb85c6af65746
alexey1254 <alxkill54@gmail.com>
2022-05-23 18:15:16 +0100

    Se implementan metodos

--- tema07/src/main/java/es/alejandro/programacion/modelo/ModeloDAO.java
+++ tema07/src/main/java/es/alejandro/programacion/modelo/ModeloDAO.java
@@ -1,5 +1,6 @@
 package es.alejandro.programacion.modelo;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 /**
@@ -8,16 +9,37 @@ import java.sql.ResultSet;
 public class ModeloDAO {
 
     private Producto registroProducto(ResultSet rs) {
+        Producto producto = new Producto();
+        producto.setCodigo(rs.getInt("codigo"));
+        producto.setNombre(rs.getString("nombre"));
 
     }
 
     public static int numProductos() throws Exception {
-        ResultSet consulta = Conexion.consulta("select count(nombre) from producto;");
-
+        ResultSet consulta = Conexion.consulta("select count(*) as cantidad from producto;");
+        PreparedStatement ps=Conexion.getPreparedStatement(sql);
+        ps.execute();
+        ps.getResultSet();
+        ResultSet rs=ps.getResultSet();
+        if(rs.next()) {
+            return rs.getInt("cantidad");
+        } else {
+            throw new Exception("numProductos. No se ha podido realizar la consulta");
+            return -1;
+        }
     }
 
     public static Producto getProductos(int page, int tamanioPage) {
-
+        ModeloDAO modeloDAO = new ModeloDAO();
+        String sql="Select * from producto where codigo=" + codigo;
+        PreparedStatement ps = Conexion.getPreparedStatement(sql);
+        ps.execute();
+        ResultSet rs=ps.getResultSet();
+        if(rs.next()) {
+            return modeloDAO.registroProducto(rs);
+        } else {
+            throw new Exception("getProductos. No ha podido realizarse la consulta");
+        }
     }
 
     public static Producto getProducto(int codigo) {
@@ -33,7 +55,14 @@ public class ModeloDAO {
     }
 
     public static int borrarProducto(int codigo) {
-
+        try{
+            String sql="DELETE FROM producto WHERE codigo="+codigo;
+            PreparedStatement ps=Conexion.getPreparedStatement(sql);
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            System.err.println("No se ha podido borrar el producto");
+            return -1;
+        }
     }
 
     public static void main(String[] args) throws Exception {
