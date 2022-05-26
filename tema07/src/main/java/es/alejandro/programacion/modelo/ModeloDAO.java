6f157a3c4739e1d95d13f908f0c2e3ba200ff09e
alexey1254 <alxkill54@gmail.com>
2022-05-26 09:41:52 +0100

    xx

--- tema07/src/main/java/es/alejandro/programacion/modelo/ModeloDAO.java
+++ tema07/src/main/java/es/alejandro/programacion/modelo/ModeloDAO.java
@@ -2,19 +2,29 @@ package es.alejandro.programacion.modelo;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Alejandro
  */
 public class ModeloDAO {
 
+<<<<<<< HEAD
     // private Producto registroProducto(ResultSet rs) {
     Producto producto = new Producto();
     // producto.setCodigo(rs.getInt("codigo"));
     // producto.setNombre(rs.getString("nombre"));
+=======
+    private static Producto registroProducto(ResultSet rs) {
+        Producto producto = new Producto();
+        producto.setCodigo(rs.getInt("codigo"));
+        producto.setNombre(rs.getString("nombre"));
+>>>>>>> 10a694b4bf20acacaa24804183572cf03ee07d60
 
     // }
 
+<<<<<<< HEAD
     /*
      * public static int numProductos() throws Exception {
      * ResultSet consulta =
@@ -58,6 +68,55 @@ public class ModeloDAO {
      * 
      * }
      */
+=======
+    public static int numProductos() throws Exception {
+        ResultSet consulta = Conexion.consulta("select count(*) as cantidad from producto;");
+        PreparedStatement ps=Conexion.getPreparedStatement("select * from producto");
+        ps.execute();
+        ps.getResultSet();
+        ResultSet rs=ps.getResultSet();
+        if(rs.next()) {
+            return rs.getInt("cantidad");
+        } else {
+            throw new Exception("numProductos. No se ha podido realizar la consulta");
+        }
+    }
+
+    public static List<Producto> getProductos(int page, int tamanioPage) throws Exception {
+        List<Producto> productos = new ArrayList<>();
+        ModeloDAO modeloDAO = new ModeloDAO();
+        String sql = "select * from producto limit"+(page-1*tamanioPage)+", "+tamanioPage;
+        PreparedStatement ps= Conexion.getPreparedStatement(sql);
+        ps.execute();
+        ResultSet rs = ps.getResultSet();
+        while(rs.next()) {
+            productos.add(modeloDAO.registroProducto(rs));
+        }
+        return productos;
+    }
+
+    public static Producto getProducto(int codigo) throws Exception {
+
+        String sql="Select * from producto where codigo="+codigo;
+        PreparedStatement ps = Conexion.getPreparedStatement(sql);
+        ps.execute();
+        ResultSet rs = ps.getResultSet();
+        if(rs.next()) {
+            return ModeloDAO.registroProducto(rs);
+        } else {
+            throw new Exception("GetProducto. No ha podido realizarse la consulta");
+        }
+
+    }
+
+    public static int actualizarProducto(Producto producto) {
+
+    }
+
+    public static int insertarProducto(Producto producto) {
+
+    }
+>>>>>>> 10a694b4bf20acacaa24804183572cf03ee07d60
 
     public static int borrarProducto(int codigo) {
         try {
